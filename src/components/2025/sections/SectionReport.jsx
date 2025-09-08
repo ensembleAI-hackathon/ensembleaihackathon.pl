da669d95530ee96f50e968424e31b258e625dd85
Jakub Worek <worekkuba@gmail.com>
2025-09-08 18:58:00 +0200

    #1 add available videos

--- src/components/2025/sections/SectionReport.jsx
+++ src/components/2025/sections/SectionReport.jsx
@@ -10,6 +10,26 @@ const SectionReport = () => {
         {t("reportFromTheEvent")} <strong>_</strong>
       </h3>
       <div className="report">
+        <div className="video-container">
+          <iframe
+            src="https://www.youtube.com/embed/KuHh_BRdWwo?si=z1qAnRrurgJEmyl7"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="video-container">
+          <iframe
+            src="https://www.youtube.com/embed/tmEnQVzp-eM?si=kKQY4v6PUSHx5gdb"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
         <Link to="/2025/gallery">
           <button className="gallery-button">{t("gallery2025")}</button>
         </Link>
